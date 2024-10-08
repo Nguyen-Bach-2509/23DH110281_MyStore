24210a49c19284bffb60ce340811247281f06bfc
Admin <Admin@MSI>
2024-10-08 15:11:55 +0700

    my store 15/10 Add project files.

---
+++ 23DH110281_MyStore/Areas/Admin/Controllers/HomeController1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _23DH110281_MyStore.Areas.Admin.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        // GET: Admin/HomeController1
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
